741110ea116f35884c5fa9cbcc55be18f74789f3
Pragathika <pragathikasupi@gmail.com>
2023-10-02 21:34:29 +0530

    Fixed hardcoded secret vulnerability in config. file

--- frontend/src/component/User/config.js
+++ frontend/src/component/User/config.js
@@ -5,16 +5,18 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWVdpZYPVqHcwFsxcnsDjSf5pgGHlTRc8",
-  authDomain: "ead-spm-project.firebaseapp.com",
-  projectId: "ead-spm-project",
-  storageBucket: "ead-spm-project.appspot.com",
-  messagingSenderId: "452444343416",
-  appId: "1:452444343416:web:fd7965d0c1023fbad3318d"
+  //Fixed hardcoded secret vulnerability
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth =getAuth(app)
+const auth = getAuth(app)
 const provider = new GoogleAuthProvider();
-export {auth,provider};
+export { auth, provider };
